c8dcfc6d05354d0795f46627c912abb85adec904
Dries De Peuter <dries@stenic.io>
2021-02-21 18:26:55 +0100

    feature(cd): Add yq plugin

---
+++ src/io/stenic/jpipe/plugin/CDInfraAsCodePlugin.groovy
@@ -0,0 +1,70 @@
+package io.stenic.jpipe.plugin
+
+import io.stenic.jpipe.event.Event
+import org.jenkinsci.plugins.scriptsecurity.sandbox.RejectedAccessException
+
+class CDInfraAsCodePlugin extends Plugin {
+
+    private String credentialId;
+    private String repository;
+    private String branch;
+    private String filePath;
+    private String yamlPath;
+    private String cdBranch;
+    private String gitUser;
+    private String gitEmail;
+
+    CDInfraAsCodePlugin(Map opts = [:]) {
+        this.repository = opts.get('repository', '');
+        this.credentialId = opts.get('credentialId', '');
+        this.cdBranch = opts.get('cdBranch', 'main');
+        this.branch = opts.get('branch', 'master');
+        this.yamlPath = opts.get('yamlPath', '.image.tag');
+        this.filePath = opts.get('filePath', 'values.yaml');
+        this.gitUser = opts.get('gitUser', 'jpipe-ci');
+        this.gitEmail = opts.get('gitEmail', 'jpipe@stenic.io');
+    }
+
+    public Map getSubscribedEvents() {
+        return [
+            "${Event.DEPLOY}": [
+                [{ event -> this.doYamlUpdate(event) }, 10],
+            ],
+        ]
+    }
+
+    public Boolean doYamlUpdate(Event event) {
+        if (event.env.BRANCH_NAME != this.cdBranch) {
+            event.script.println("Skipping CDInfraAsCodePlugin")
+            return true;
+        }
+
+        if (this.credentialId == '') {
+            this.credentialId = event.script.scm.getUserRemoteConfigs()[0].getCredentialsId();
+        }
+
+        event.script.dir( "${System.currentTimeMillis()}" ) {
+            event.script.git(
+                url: this.repository,
+                branch: this.branch,
+                credentialsId: this.credentialId,
+                changelog: false
+            );
+
+            event.script.docker.image("mikefarah/yq:4").inside("--entrypoint=''") {
+                event.script.sh "yq eval '${this.yamlPath} = \"${event.version}\"' ${this.filePath}";
+            }
+
+            event.script.sshagent(credentials: [this.credentialId]) {
+                event.script.sh "git config user.email '${this.gitEmail}'"
+                event.script.sh "git config user.name '${this.gitUser}'"
+
+                event.script.sh "git add ${this.filePath}"
+                event.script.sh "git commit -m 'Update version ${this.filePath} to ${event.version}'"
+                event.script.sh "git push origin ${this.branch}"
+            }
+        }
+
+        return true
+    }
+}
