777f44062ab1dfe5733152308dd64662b131f144
Dries De Peuter <dries@stenic.io>
2021-02-21 18:26:55 +0100

    feature(cd): Add yq plugin

--- src/io/stenic/jpipe/plugin/SonarQubePlugin.groovy
+++ src/io/stenic/jpipe/plugin/SonarQubePlugin.groovy
@@ -1,15 +1,18 @@
 package io.stenic.jpipe.plugin
 
 import io.stenic.jpipe.event.Event
+import java.lang.Exception
 
 class SonarQubePlugin extends Plugin {
 
     private String credentialsId;
     private String projectKey;
+    private Boolean allowFailure;
     
     SonarQubePlugin(Map opts = [:]) {
         this.credentialsId = opts.get('credentialId', 'sonarqube-token');
         this.projectKey = opts.get('projectKey');
+        this.allowFailure = opts.get('allowFailure', true);
     }
 
     public Map getSubscribedEvents() {
@@ -21,9 +24,15 @@ class SonarQubePlugin extends Plugin {
     }
 
     public void doSonarScan(Event event) {
-        event.script.docker.image('sonarsource/sonar-scanner-cli:latest').inside('--privileged') {
-            event.script.withSonarQubeEnv(credentialsId: this.credentialsId) {
-                event.script.sh "sonar-scanner -Dsonar.projectKey=${this.projectKey}"
+        try {
+            event.script.docker.image('sonarsource/sonar-scanner-cli:latest').inside('--privileged') {
+                event.script.withSonarQubeEnv(credentialsId: this.credentialsId) {
+                    event.script.sh "sonar-scanner -Dsonar.projectKey=${this.projectKey}"
+                }
+            }
+        } catch (Exception e) {
+            if (!this.allowFailure) {
+                throw e;
             }
         }
     }
