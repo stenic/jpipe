9ed5db11c4d96ebe4f13889ac81bc27407d9a97c
Dries De Peuter <dries@stenic.io>
2021-04-14 23:33:27 +0200

    feat(trigger): Allow passing custom params

--- src/io/stenic/jpipe/plugin/TriggerBuildPlugin.groovy
+++ src/io/stenic/jpipe/plugin/TriggerBuildPlugin.groovy
@@ -7,11 +7,15 @@ class TriggerBuildPlugin extends Plugin {
     private String jobName;
     private String eventName;
     private Integer eventWeight;
+    private Map params;
+    private Boolean addVersionParam;
     
     TriggerBuildPlugin(Map opts = [:]) {
         this.jobName = opts.get('jobName');
         this.eventName = opts.get('eventName', 'Deploy');
         this.eventWeight = opts.get('eventWeight', 20);
+        this.params = opts.get('params', [:]);
+        this.addVersionParam = opts.get('addVersion', true);
     }
 
     public Map getSubscribedEvents() {
@@ -23,9 +27,15 @@ class TriggerBuildPlugin extends Plugin {
     }
 
     public void doTriggerBuild(Event event) {
+        def params = [];
+        this.params.each { params += event.script.string(name: it.key, value: it.value) }
+        if (this.addVersionParam) {
+            params += event.script.string(name: 'version', value: event.version)
+        }
+
         event.script.build(
             job: this.jobName,
-            parameters: [event.script.string(name: 'version', value: event.version)]
+            parameters: params
         )
     }
 }
