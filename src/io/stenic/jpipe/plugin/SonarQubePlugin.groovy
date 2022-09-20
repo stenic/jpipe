5cd86cdc905124d9f514a23a9cc72392b61c881a
Dries De Peuter <dries@stenic.io>
2022-09-20 11:49:59 +0200

    feat(sonarqube): Enhance sonarqube options

--- src/io/stenic/jpipe/plugin/SonarQubePlugin.groovy
+++ src/io/stenic/jpipe/plugin/SonarQubePlugin.groovy
@@ -8,11 +8,19 @@ class SonarQubePlugin extends Plugin {
     private String credentialsId;
     private String projectKey;
     private Boolean allowFailure;
+    private String dockerImage;
+    private String command;
+    private ArrayList<String> extraArguments;
+    private ArrayList<String> dockerArguments;
 
     SonarQubePlugin(Map opts = [:]) {
         this.credentialsId = opts.get('credentialId', 'sonarqube-token');
         this.projectKey = opts.get('projectKey');
         this.allowFailure = opts.get('allowFailure', true);
+        this.dockerImage = opts.get('dockerImage', 'sonarsource/sonar-scanner-cli:latest');
+        this.command = opts.get('command', 'sonar-scanner');
+        this.extraArguments = opts.get('extraArguments', []);
+        this.dockerArguments = opts.get('dockerArguments', ['--privileged']);
     }
 
     public Map getSubscribedEvents() {
@@ -25,9 +33,9 @@ class SonarQubePlugin extends Plugin {
 
     public void doSonarScan(Event event) {
         try {
-            event.script.docker.image('sonarsource/sonar-scanner-cli:latest').inside('--privileged') {
+            event.script.docker.image(this.dockerImage).inside(this.dockerArguments.join(' ')) {
                 event.script.withSonarQubeEnv(credentialsId: this.credentialsId) {
-                    event.script.sh "sonar-scanner -Dsonar.projectKey=${this.projectKey} -Dsonar.projectVersion=${event.version}"
+                    event.script.sh "${this.command} -Dsonar.projectKey=${this.projectKey} -Dsonar.projectVersion=${event.version} ${this.extraArguments.join(' ')}"
                 }
             }
         } catch (Exception e) {
