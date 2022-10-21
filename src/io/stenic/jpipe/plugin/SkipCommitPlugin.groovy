e06a04ec5e13ddbd2341f7ce4a5d02ea4329a751
Dries De Peuter <dries@stenic.io>
2022-10-21 10:05:17 +0200

    fix(skip-ci): Allow users to re-trigger the build

--- src/io/stenic/jpipe/plugin/SkipCommitPlugin.groovy
+++ src/io/stenic/jpipe/plugin/SkipCommitPlugin.groovy
@@ -16,9 +16,16 @@ class SkipCommitPlugin extends Plugin {
     }
 
     public Boolean doSkipCommit(Event event) {
+
         // If the last commit includes [ci skip], do not proceed.
         def commitMsg = event.script.sh(script: "git log -n 1 HEAD", returnStdout: true)
+
         if (commitMsg.matches(/(?ms)(.*\[(skip ci|ci skip)\].*)/)) {
+            // Check if the build was started by a user.
+            if (event.script.currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause) != null) {
+                event.script.println = 'Triggered by user, ignoring [skip ci]'
+                return true
+            }
             event.script.currentBuild.description = 'Skipped by [skip ci]'
 
             // try {
@@ -29,7 +36,7 @@ class SkipCommitPlugin extends Plugin {
             //     event.script.currentBuild.getRawBuild().delete()
             //     event.script.currentBuild.getRawBuild().setResult(Result.fromString(event.script.currentBuild.getPreviousBuild().result))
             // } catch (RejectedAccessException e) {}
-            
+
             throw new FlowInterruptedException(Result.fromString(event.script.currentBuild.getPreviousBuild().result), true, new UserInterruption(event.script.env.BUILD_USER_ID))
         }
 
